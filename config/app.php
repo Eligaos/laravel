acb94ceb4ab5974953b49cf66a19ec6298690fc1
Mickael <mickaelgomes90@gmail.com>
2015-12-30 22:47:43 +0000

    commit
    commit problems

--- config/app.php
+++ config/app.php
@@ -138,8 +138,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
-       // Collective\Html\HtmlServiceProvider::class,
+        //Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
         /*
          * Application Service Providers...
          */
